cd19c4725ec31915639c7376dc2e88fa386ebabf
Dmytro Mariukhna <maryukhna05@gmail.com>
2025-06-25 22:13:33 +0300

    added constants to the code

--- homework_5/delete_data_in_db.py
+++ homework_5/delete_data_in_db.py
@@ -1,5 +1,5 @@
 import logging
-from homework_5.consts import LOGGER_NAME
+from homework_5.consts import LOGGER_NAME, SQL_DELETE_USER, SQL_DELETE_BANK, SQL_DELETE_ACCOUNT
 from homework_5.db_connection_decorator import db_connection
 
 
@@ -16,7 +16,7 @@ def delete_user(user_id: int, cursor=None):
     """
     logger = logging.getLogger(LOGGER_NAME)
     try:
-        cursor.execute('DELETE FROM User WHERE id = ?', (user_id,))
+        cursor.execute(SQL_DELETE_USER, (user_id,))
         logger.info('Successfully deleted user with id %s', user_id)
         return f'Successfully deleted user with id {user_id}'
     except Exception as e:
@@ -37,7 +37,7 @@ def delete_bank(bank_id: int, cursor=None):
     """
     logger = logging.getLogger(LOGGER_NAME)
     try:
-        cursor.execute('DELETE FROM Bank WHERE id = ?', (bank_id,))
+        cursor.execute(SQL_DELETE_BANK, (bank_id,))
         logger.info('Successfully deleted bank with id %s', bank_id)
         return f'Successfully deleted bank with id {bank_id}'
     except Exception as e:
@@ -58,7 +58,7 @@ def delete_account(account_id: int, cursor=None):
     """
     logger = logging.getLogger(LOGGER_NAME)
     try:
-        cursor.execute('DELETE FROM Account WHERE id = ?', (account_id,))
+        cursor.execute(SQL_DELETE_ACCOUNT, (account_id,))
         logger.info('Successfully deleted account with id %s', account_id)
         return f'Successfully deleted account with id {account_id}'
     except Exception as e:
